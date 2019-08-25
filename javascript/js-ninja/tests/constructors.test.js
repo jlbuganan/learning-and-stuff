76a5679599d9145d68e9f8d462b0419567563b12
Jerms Buganan <liljerms@gmail.com>
2019-08-24 21:24:54 -0700

    learn more about invoking constructors

--- javascript/js-ninja/tests/constructors.test.js
+++ javascript/js-ninja/tests/constructors.test.js
@@ -15,18 +15,89 @@ test('snafu should be an instance of Fubar', () => {
   expect(snafu.concatenated()).toBe("Greetings Planet");
 });
 
-// Bad: fubar will be undefined, and this gets set to the global environment so the properties _foo and _bar exist
+// Bad: fubar will be undefined, and 'this' gets set to the global environment so the properties _foo and _bar exist
 var fubar = Fubar("Salutations", "Realm");
-test('fubar should be undefined', () => {
+test('fubar is undefined??', () => {
   expect(fubar).toBeUndefined();
   expect(_foo).toEqual("Salutations");
   expect(_bar).toEqual("Realm");
   expect(_this()).toEqual(window);
 });
 
-// An attempt to fix the above problem
+// An attempt to fix the above problem with auto-instantiation
+var fubar2 = Fubar2("Hello.", "is it me you're looking for?");
 function Fubar2 (foo, bar) {
   "use strict";
-  this._foo = foo;
-  this._bar = bar;
+
+  var obj, ret;
+
+  if (this instanceof Fubar2) {
+    this._foo2 = foo;
+    this._bar2 = bar;
+  } else {
+    return new Fubar2(foo, bar);
+  }
 }
+Fubar2.prototype.concatenated = function() {
+  return `${this._foo2} ${this._bar2}`;
+};
+
+test('fubar2 should be instantiated correctly even without new', () => {
+  expect(fubar2 instanceof Fubar2).toBeTruthy();
+});
+
+
+// Experimentation
+function logsArguments (fn) {
+  return function() {
+    console.log.apply(this, arguments);
+    return fn.apply(this, arguments);
+  };
+}
+
+function sum2 (a, b) {
+  return a + b;
+}
+
+var logsSum = logsArguments(sum2);
+logsSum(2, 3);
+
+var LoggingFubar2 = logsArguments(Fubar2);
+var snafu2 = new LoggingFubar2("what", "the");
+snafu2.concatenated();
+
+
+// Fix the problem with overloading
+function Fubar3 (foo, bar) {
+  "use strict";
+
+  if (this instanceof Fubar3) {
+    this._foo3 = foo;
+    this._bar3 = bar;
+  } else {
+    return arguments[0] instanceof Fubar3;
+  }
+}
+
+var snafu3 = new Fubar3("Holy", "mother of god");
+
+test('invoking Fubar3 without new should return true', () => {
+  expect(Fubar3({})).toBeFalsy();
+  expect(Fubar3(snafu3)).toBeTruthy();
+});
+
+// Throw an exception
+function Fubar4 (foo, bar) {
+  "use strict";
+
+  if (!(this instanceof Fubar)) {
+    throw new Error("Needs to be called with the 'new' keyword!");
+  }
+
+  this._foo4 = foo;
+  this._bar4 = bar;
+}
+
+test('invoking Fubar4 without new should throw an exception', () => {
+  expect(() => Fubar4("Who","did it")).toThrow(Error);
+});
